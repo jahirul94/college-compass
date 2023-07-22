45644be752e7bfea540e16dc642e1195ca7a0316
jahirul94 <jahirulislam41185@gmail.com>
2023-07-23 00:17:25 +0600

    added gallery section

--- src/pages/Home/Gallery/Gallery.jsx
+++ src/pages/Home/Gallery/Gallery.jsx
@@ -1,8 +1,23 @@
+import useCollege from "../../../Hooks/useCollege";
 
 const Gallery = () => {
+    const [colleges] = useCollege();
+    console.log(colleges);
     return (
-        <div>
-            
+        <div className="min-h-screen my-10 pt-32">
+            <div>
+                <h2 className="text-4xl font-bold text-center mb-14">Academic Achievers</h2>
+            </div>
+            <div className="grid gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+                {
+                    colleges?.map(clg => <div key={clg._id} className="card card-compact bg-base-100 shadow-xl">
+                    <figure><img src={clg.gra_pic} alt="students pic" className="h-72"/></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">{clg.collegeName}'s Dreamers</h2>
+                    </div>
+                </div>)
+                }
+            </div>
         </div>
     );
 };
